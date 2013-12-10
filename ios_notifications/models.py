5493fa930988857ffd0884f5be0aace687f54501
Brian Zhao <brian@bex.com.au>
2013-12-10 13:24:54 +1100

    Added error handling on disconnection.

--- ios_notifications/models.py
+++ ios_notifications/models.py
@@ -3,6 +3,7 @@ import socket
 import struct
 import errno
 import json
+import logging
 from binascii import hexlify, unhexlify
 
 from django.db import models
@@ -19,6 +20,8 @@ import OpenSSL
 from OpenSSL.SSL import SysCallError
 
 from .exceptions import NotificationPayloadSizeExceeded, InvalidPassPhrase
+
+logger = logging.getLogger(__name__)
 
 
 class BaseService(models.Model):
@@ -60,8 +63,11 @@ class BaseService(models.Model):
         Closes the SSL socket connection.
         """
         if self.connection is not None:
-            self.connection.shutdown()
-            self.connection.close()
+            try:
+                self.connection.shutdown()
+                self.connection.close()
+            except Exception:
+                logger.error("iOS push notification disconnect error." , exc_info=sys.exc_info())
 
     class Meta:
         abstract = True
